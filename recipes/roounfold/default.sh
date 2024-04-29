8935b59d9317048bd4fc333b05cee5b5dcc7cfe1
elesser <elesser@umich.edu>
2024-04-29 16:57:43 +0200

    Update roounfold to allow different versions

--- recipes/roounfold/default.sh
+++ recipes/roounfold/default.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 export RUGITREPO=https://gitlab.cern.ch/RooUnfold/RooUnfold.git
-roounfold_version=master
+roounfold_version=2.0.1
 
-#ezrasru=$(get_opt "ezra" $@)
-#if [ ! -z ${ezrasru} ]; then
-#    export RUGITREPO=https://gitlab.cern.ch/elesser/RooUnfold.git
-#    roounfold_version=master
-#fi
+ezrasru=$(get_opt "ezra" $@)
+if [ ! -z ${ezrasru} ]; then
+    export RUGITREPO=https://gitlab.cern.ch/elesser/RooUnfold.git
+    roounfold_version=master
+fi
 
 echo "[i] Using RooUnfold at ${RUGITREPO} ${roounfold_version}"
 echo "    version=${roounfold_version}"
@@ -17,7 +17,8 @@ git clone ${RUGITREPO}
 srcdir={{workdir}}/RooUnfold
 build_dir={{workdir}}/build
 if [ ! {{roounfold_version}} == master ]; then
-    git checkout {{roounfold_version}}
+    cd {{srcdir}}
+    git checkout tags/{{roounfold_version}} -b {{roounfold_version}}-dev
 fi
 cd {{build_dir}}
 cmake {{srcdir}} -DCMAKE_INSTALL_PREFIX={{prefix}} -DCMAKE_BUILD_TYPE=Release
